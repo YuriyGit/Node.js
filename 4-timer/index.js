ffc86d1f92143b2e9f4d11a79b35e51ca433eba7
Yuriy <bomawo3052@zamaneta.com>
2024-09-22 17:43:25 +0400

    fix

--- 4-timer/index.js
+++ 4-timer/index.js
@@ -3,24 +3,30 @@ const minutes = process.argv[3]
 const seconds = process.argv[4]
 
 function timer(hours, minutes, seconds) {
-    let secondsInEnteredHours = 0;
-    let secondsInEnteredMinutes = 0;
-    let enteredSeconds = 0;
-    if (hours > 0) {
-        secondsInEnteredHours = hours * 60 * 60;
+    if (isNaN(hours)) {
+        console.log('Часы введенены не верно! Введите число!')
+        return
     }
-    if (minutes > 0) {
-        secondsInEnteredMinutes = minutes * 60
+    if (isNaN(minutes)) {
+        console.log('Минуты введенены не верно! Введите число!')
+        return
     }
-    if (seconds > 0) {
-        enteredSeconds = Number(seconds)
+    if (isNaN(seconds)) {
+        console.log('Секунды введенены не верно! Введите число!')
+        return
     }
-    return secondsInEnteredHours + secondsInEnteredMinutes + enteredSeconds;
+
+    let secondsInEnteredHours = Number(hours) * 60 * 60;
+    let secondsInEnteredMinutes = Number(minutes) * 60;
+    let enteredSeconds = Number(seconds);
+
+    const time = secondsInEnteredHours + secondsInEnteredMinutes + enteredSeconds;
+
+    console.log(`Таймер сработает через ${time} секунд`)
+
+    setTimeout(() => {
+        console.log(`Таймер сработал спустя ${time} секунд`)
+    }, time * 1000)
 }
 
-console.log(`таймер сработает через ${timer(hours, minutes, seconds)} секунд`)
-
-setTimeout(() => {
-    console.log(`таймер сработал спустя ${timer(hours, minutes, seconds)} секунд`)
-}, timer(hours, minutes, seconds) * 1000)
-
+timer(hours, minutes, seconds);
